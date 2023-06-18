74c6d6926954a9805a0c94c4d3ee8b9431ae7cfb
AMANS <92840854+ARNAB-BOTMAS@users.noreply.github.com>
2023-06-19 02:56:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,25 +4,29 @@ import urllib.parse
 
 def git():
     # Change to the directory where the Python file is located
-    subprocess.run('git init', shell=True)
-    subprocess.run('git config --global user.email "arnabmondal203@gmail.com"', shell=True)
-    subprocess.run('git config --global user.name "ARNAB-BOTMAS"', shell=True)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # Initialize a Git repository
+    subprocess.run('git init', shell=True)
+
+    # Set Git configuration
+    subprocess.run('git config --global user.email "you@example.com"', shell=True)
+    subprocess.run('git config --global user.name "Your Name"', shell=True)
 
     # Commit message
     commit_message = "Automatic commit"
 
     # Git commands
     git_commands = [
-        f"git remote add origin https://ARNAB-BOTMAS:{urllib.parse.quote('Arnab@8016')}@github.com/ARNAB-BOTMAS/sri_python.git",
         "git add .",
         f'git commit -m "{commit_message}"',
-        # "git pull origin main --allow-unrelated-histories",
-        "git push origin main"
+        f'git remote add origin https://github.com/ARNAB-BOTMAS/sri_python.git',
+        f'git config --local credential.helper "!f() {{ echo username=ARNAB-BOTMAS; echo password=$1; }}; f"',
+        "git push -u origin main"
     ]
 
     # Execute git commands
     for command in git_commands:
-        subprocess.run(command, shell=True)
+        subprocess.run(command, shell=True, check=True, input="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".encode())
 
-# git()
+git()
