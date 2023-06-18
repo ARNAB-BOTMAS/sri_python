5169e667368b53af21cef4fbd55c3cd942e68b89
Arnab Mondal <arnabmondal203@gmail.com>
2023-06-19 01:25:22 +0530

    Automatic commit

--- github_repo.py
+++ github_repo.py
@@ -1,10 +1,6 @@
 import os
 import subprocess
 
-# Run git config commands to set user email and name
-subprocess.run('git config --global user.email "arnabmondal203@gmail.com"', shell=True)
-subprocess.run('git config --global user.name "Arnab Mondal"', shell=True)
-
 def git():
     # Change to the directory where the Python file is located
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -22,4 +18,3 @@ def git():
     # Execute git commands
     for command in git_commands:
         subprocess.run(command, shell=True)
-
