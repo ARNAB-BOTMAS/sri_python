f11418e0ffa9b4b163d1aa6bc9e5b0080a1d3218
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-19 02:28:19 +0530

    Automatic commit

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import subprocess
 
 def git():
     # Change to the directory where the Python file is located
-    subprocess.run('git config --global user.email "arnabmondal203@gmail.com"', shell=True)
-    subprocess.run('git config --global user.name "ARNAB-BOTMAS"', shell=True)
+    # subprocess.run('git config --global user.email "arnabmondal203@gmail.com"', shell=True)
+    # subprocess.run('git config --global user.name "ARNAB-BOTMAS"', shell=True)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     # Commit message
@@ -12,10 +12,10 @@ def git():
 
     # Git commands
     git_commands = [
-        "git remote add origin https://github.com/ARNAB-BOTMAS/sri_python.git",
+        # "git remote add origin https://github.com/ARNAB-BOTMAS/sri_python.git",
         "git add .",
         f'git commit -m "{commit_message}"',
-        "git pull origin main --allow-unrelated-histories",
+        # "git pull origin main --allow-unrelated-histories",
         "git push -u origin main"
     ]
 
