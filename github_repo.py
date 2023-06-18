c9799d44a22cfa8c1c6e725a03b0a615e5237054
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-19 01:30:24 +0530

    Automatic commit

--- github_repo.py
+++ github_repo.py
@@ -3,6 +3,8 @@ import subprocess
 
 def git():
     # Change to the directory where the Python file is located
+    subprocess.run('git config --global user.email "arnabmondal203@gmail.com"', shell=True)
+    subprocess.run('git config --global user.name "ARNAB-BOTMAS"', shell=True)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     # Commit message
